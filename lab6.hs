7236390a4a158d6ae050a5103f0afc962c48811f
eli-hu <eli.hu@sbcglobal.net>
2014-12-16 18:10:52 -0600

    Lab6 working through ex15.

--- lab6.hs
+++ lab6.hs
@@ -14,13 +14,18 @@ data Rose a = a :> [Rose a] deriving Show
 -- ===================================
 
 root :: Rose a -> a
-root = error "you have to implement root"
+root (p :> cs) = p
 
 children :: Rose a -> [Rose a]
-children = error "you have to implement children"
+children (p :> cs) = cs
+
+tree0 = 'x' :> map (flip (:>) []) ['a'..'x']
+ex0 = length $ children tree0
+
+tree1 = 'x' :> map (\c -> c :> []) ['a'..'A']
+ex1 = length (children tree1)
 
 xs = 0 :> [1 :> [2 :> [3 :> [4 :> [], 5 :> []]]], 6 :> [], 7 :> [8 :> [9 :> [10 :> []], 11 :> []], 12 :> [13 :> []]]]
-
 ex2 = root . head . children . head . children . head . drop 2 $ children xs
 
 -- ===================================
@@ -28,10 +33,23 @@ ex2 = root . head . children . head . children . head . drop 2 $ children xs
 -- ===================================
 
 size :: Rose a -> Int
-size = error "you have to implement size"
+size (p :> cs) = 1 + (sum $ map size cs)
 
 leaves :: Rose a -> Int
-leaves = error "you have to implement leaves"
+leaves (p :> []) = 1
+leaves (p :> cs) = sum $ map leaves cs
+
+tree3 = 1 :> map (\c -> c :> []) [1..5]
+ex3 = size tree3
+
+tree4 = 1 :> map (\c -> c :> []) [1..5]
+ex4 = size . head . children $ tree4
+
+tree5 = 1 :> map (\c -> c :> []) [1..5]
+ex5 = leaves tree5
+
+tree6 = 1 :> map (\c -> c :> []) [1..5]
+ex6 = product (map leaves (children tree6))
 
 ex7 = (*) (leaves . head . children . head . children $ xs) (product . map size . children . head . drop 2 . children $ xs)
 
@@ -40,7 +58,13 @@ ex7 = (*) (leaves . head . children . head . children $ xs) (product . map size 
 -- ===================================
 
 instance Functor Rose where
-  fmap = error "you have to implement fmap for Rose"
+  fmap f (p :> cs) = f p :> map (fmap f) cs
+
+tree8 = 1 :> map (\c -> c :> []) [1..5]
+ex8 = size (fmap leaves (fmap (:> []) tree8))
+
+f :: Rose a -> Rose a
+f r = fmap head $ fmap (\x -> [x]) r
 
 ex10 = round . root . head . children . fmap (\x -> if x > 0.5 then x else 0) $ fmap (\x -> sin(fromIntegral x)) xs
 
@@ -52,21 +76,23 @@ class Monoid m where
   mempty :: m
   mappend :: m -> m -> m
 
-newtype Sum a = Sum a
-newtype Product a = Product a
+newtype Sum a = Sum a deriving Show
+newtype Product a = Product a deriving Show
 
 instance Num a => Monoid (Sum a) where
-  mempty = error "you have to implement mempty for Sum"
-  mappend = error "you have to implement mappend for Sum"
+  mempty = Sum 0
+  mappend (Sum x) (Sum y) = Sum (x + y)
 
 instance Num a => Monoid (Product a) where
-  mempty = error "you have to implement mempty for Product"
-  mappend = error "you have to implement mappend for Product"
+  mempty = Product 1
+  mappend (Product x) (Product y) = Product (x * y)
 
 unSum :: Sum a -> a
-unSum = error "you have to implement unSum"
+unSum (Sum x) = x
 unProduct :: Product a -> a
-unProduct = error "you have to implement unProduct"
+unProduct (Product x) = x
+
+ex11 = unProduct (Product 6 `mappend` (Product . unSum $ Sum 3 `mappend` Sum 4))
 
 num1 = mappend (mappend (Sum 2) (mappend (mappend mempty (Sum 1)) mempty)) (mappend (Sum 2) (Sum 1))
 
@@ -74,21 +100,30 @@ num2 = mappend (Sum 3) (mappend mempty (mappend (mappend (mappend (Sum 2) mempty
 
 ex13 = unSum (mappend (Sum 5) (Sum (unProduct (mappend (Product (unSum num2)) (mappend (Product (unSum num1)) (mappend mempty (mappend (Product 2) (Product 3))))))))
 
+
 -- ===================================
 -- Ex. 14-15
 -- ===================================
 
+-- mempty
+
 class Functor f => Foldable f where
   fold :: Monoid m => f m -> m
   foldMap :: Monoid m => (a -> m) -> (f a -> m)
-  foldMap = error "you have to implement foldMap"
+  foldMap = error "eek"
 
 instance Foldable Rose where
-  fold = error "you have to implement fold for Rose"
+  fold (m :> cs) = mappend m (foldr (mappend . fold) mempty cs)
+
+tree14 = 1 :> [2 :> [], 3 :> [4 :> []]]
+tree14' = fmap Product tree14
+ex14 = unProduct $ fold tree14'
 
 sumxs = Sum 0 :> [Sum 13 :> [Sum 26 :> [Sum (-31) :> [Sum (-45) :> [], Sum 23 :> []]]], Sum 27 :> [], Sum 9 :> [Sum 15 :> [Sum 3 :> [Sum (-113) :> []], Sum 1 :> []], Sum 71 :> [Sum 55 :> []]]]
 
 ex15 = unSum (mappend (mappend (fold sumxs) (mappend (fold . head . drop 2 . children $ sumxs) (Sum 30))) (fold . head . children $ sumxs))
+
+tree16 = 42 :> [3 :> [2:> [], 1 :> [0 :> []]]]
 
 -- ===================================
 -- Ex. 16-18
